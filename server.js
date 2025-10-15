21e7762e818405f04bbb4eebc50618047fac9f5b
cjohnson813 <camjohnson228@gmail.com>
2025-10-14 21:44:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+
 //call back function
 serveStatic = function (req, res) {
 	let fileName = '.' + url.parse(req.url).pathname;
-
-	if (fileName === './') {
-		fileName = './index.html';
-	}
-
+	
 	fs.readFile(fileName, function (err, data) {
+		// 404 error for file not found
 		if (err) {
 			res.writeHead(404, { 'Content-Type': 'text/plain' });
 			res.write('404 Not Found\n');
 			res.end();
 		}
+		// serve contents of the file
 		else {
 			res.writeHead(200, { 'Content-Type': 'text/html' });
 			res.write(data);
